671af6e9af033f88969393b6f16231ab53c470d2
Jeff Preshing <filter-github@preshing.com>
2020-08-26 10:31:44 -0400

    Link Docker-built docs to GitHub

--- repos/plywood/extra/docker/build-docker.bat
+++ repos/plywood/extra/docker/build-docker.bat
@@ -6,4 +6,4 @@ copy repos\plywood\extra\docker\Dockerfile.src data\docker\Dockerfile
 git checkout-index -a -f --prefix=data\docker\plywood\
 cd data\docker
 docker build -t plywood-docs .
-docker run -p 127.0.0.1:8080:8080/tcp plywood-docs
+rem docker run -p 127.0.0.1:8080:8080/tcp plywood-docs
