f3605d0bb3490ab492945e543eb729620d45675d
Jeff Preshing <filter-github@preshing.com>
2020-08-25 22:20:23 -0400

    Add Dockerfile to build docs on Ubuntu
    Also add Windows .bat file to automate the process

---
+++ repos/plywood/extra/docker/build-docker.bat
@@ -0,0 +1,9 @@
+pushd
+cd /D "%~dp0"
+cd ..\..\..\..
+mkdir data\docker\plywood
+copy repos\plywood\extra\docker\Dockerfile.src data\docker\Dockerfile
+git checkout-index -a -f --prefix=data\docker\plywood\
+cd data\docker
+docker build -t plywood-docs .
+docker run -p 127.0.0.1:8080:8080/tcp plywood-docs
