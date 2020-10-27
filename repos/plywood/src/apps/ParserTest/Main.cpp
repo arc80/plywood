2f28796d165afcae1d5ca5c2c97f56b2d2150b33
Jeff Preshing <filter-github@preshing.com>
2020-10-27 09:49:43 -0400

    Fix ParserTest

--- repos/plywood/src/apps/ParserTest/Main.cpp
+++ repos/plywood/src/apps/ParserTest/Main.cpp
@@ -12,49 +12,17 @@
 
 using namespace ply;
 
-bool errorExportFilter(cpp::PPVisitedFiles* visitedFiles, pylon::Node& aNode, TypedPtr any) {
-    if (any.is<cpp::ParseError>()) {
-        const cpp::ParseError* error = any.cast<cpp::ParseError>();
-        auto objNode = aNode.type.object().switchTo();
-        objNode->obj.add("type").value =
-            pylon::exportObj(TypedPtr::bind(&error->type), {visitedFiles, errorExportFilter});
-        objNode->obj.add("errorToken").value =
-            pylon::exportObj(TypedPtr::bind(&error->errorToken), {visitedFiles, errorExportFilter});
-        if (error->expected != cpp::ExpectedToken::None) {
-            objNode->obj.add("expected").value = pylon::exportObj(
-                TypedPtr::bind(&error->expected), {visitedFiles, errorExportFilter});
-        }
-        if (error->precedingToken.isValid()) {
-            objNode->obj.add("precedingToken").value = pylon::exportObj(
-                TypedPtr::bind(&error->precedingToken), {visitedFiles, errorExportFilter});
-        }
-        return true;
-    } else if (any.is<cpp::Token>()) {
-        const cpp::Token* token = any.cast<cpp::Token>();
-        auto objNode = aNode.type.object().switchTo();
-        auto valueNode = objNode->obj.add("type").value.type.text().switchTo();
-        valueNode->str = TypeResolver<cpp::Token::Type>::get()->findValue(token->type)->name.view();
+struct ParserTestSupervisor : cpp::ParseSupervisor {
+    Array<Owned<cpp::BaseError>> errors;
 
-        auto iter = visitedFiles->locationMap.findLastLessThan(token->linearLoc + 1);
-        const cpp::PPVisitedFiles::IncludeChain& chain =
-            visitedFiles->includeChains[iter.getItem().includeChainIdx];
-        PLY_ASSERT(!chain.isMacroExpansion);
-        PLY_ASSERT(chain.fileOrExpIdx == 0);
-        const cpp::PPVisitedFiles::SourceFile& srcFile =
-            visitedFiles->sourceFiles[chain.fileOrExpIdx];
-        FileLocation fileLoc = srcFile.fileLocMap.getFileLocation(
-            safeDemote<u32>(token->linearLoc - iter.getItem().offset));
-        objNode->obj.add("line").value =
-            pylon::Node::createText(String::from(fileLoc.lineNumber), {});
-        objNode->obj.add("column").value =
-            pylon::Node::createText(String::from(fileLoc.columnNumber), {});
+    virtual bool handleError(Owned<cpp::BaseError>&& err) override {
+        this->errors.append(std::move(err));
         return true;
     }
-    return false;
-}
+};
 
 void runTest(StringView testPath) {
-    String testFileContents = loadTextFile(testPath);
+    String testFileContents = FileSystem::native()->loadTextAutodetect(testPath).first;
 
     // Find the line with five dashes
     StringViewReader strViewReader{testFileContents};
@@ -77,38 +45,28 @@ void runTest(StringView testPath) {
     // Note: We could maybe avoid this copy by changing the Preprocessor so that it doesn't always
     // take memory ownership of the input file contents.
     cpp::PPVisitedFiles visitedFiles;
-    Tuple<cpp::grammar::TranslationUnit, Array<Owned<cpp::BaseError>>> parseResult =
-        cpp::parse(sourceCode, &visitedFiles, {});
+    ParserTestSupervisor visor;
+    cpp::grammar::TranslationUnit parseResult = cpp::parse(sourceCode, &visitedFiles, {}, {}, &visor);
 
-    // Convert errors to Pylon
-    //    pylon::Node root =
-    //        pylon::exportObj(TypedPtr::bind(&parseResult.second), {&visitedFiles,
-    //        errorExportFilter});
-
-    MemOutStream mout;
-    Owned<StringWriter> sw = TextFormat::platformPreference().createExporter(borrow(&mout));
-    *sw << sourceCode;
+    StringWriter sw;
+    sw << sourceCode;
     if (!sourceCode.endsWith("\n")) {
-        *sw << "\n";
+        sw << "\n";
     }
-    *sw << "-----\n";
-    //    pylon::write(sw, root);
-    //    *sw << "\n-----\n";
-    for (const cpp::BaseError* err : parseResult.second) {
-        err->writeMessage(sw, &visitedFiles);
+    sw << "-----\n";
+    for (const cpp::BaseError* err : visor.errors) {
+        err->writeMessage(&sw, &visitedFiles);
     }
-    sw->flushMem();
-    FileSystem::native()->makeDirsAndSaveIfDifferent(testPath, mout.moveToBuffer());
+    sw.flushMem();
+    FileSystem::native()->makeDirsAndSaveTextIfDifferent(testPath, sw.moveToString(), TextFormat::platformPreference());
 }
 
 void runTestSuite() {
     String testsFolder =
         NativePath::join(PLY_WORKSPACE_FOLDER, "repos/plywood/src/apps/ParserTest/tests");
-    StringWriter sw{stdOut()};
+    StringWriter sw = StdOut::createStringWriter();
     for (const DirectoryEntry& entry : FileSystem::native()->listDir(testsFolder)) {
         if (!entry.isDir && entry.name.endsWith(".txt")) {
-            // if (!entry.name.startsWith("0063"))
-            //    continue;
             sw << entry.name << '\n';
             sw.flushMem();
             runTest(NativePath::join(testsFolder, entry.name));
