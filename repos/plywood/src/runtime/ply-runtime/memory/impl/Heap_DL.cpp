arc80/plywood

commit 036ccebc8560d952803c5568a001304c93e0a022

Implement MemPage::reserve, commit & decommit
* Only supported on Windows and Linux at the moment
* Also made MemPage_Win32::free more strict
* Heap_DL no longer uses MemPage_Win32

repos/plywood/src/runtime/ply-runtime/memory/impl/Heap_DL.cpp
@@ -184,25 +184,59 @@ unsigned char _BitScanReverse(unsigned long *index, unsigned long mask);
 #define MFAIL                ((void*)(MAX_SIZE_T))
 #define CMFAIL               ((char*)(MFAIL)) /* defined for convenience */
 
-static PLY_INLINE void* ply_mmap(size_t size) {
-  void* ptr;
-  return ply::MemPage::alloc(ptr, size) ? ptr : MFAIL;
-}
+#ifndef WIN32
 
-/* For direct MMAP, use MEM_TOP_DOWN to minimize interference */
-static PLY_INLINE void* ply_direct_mmap(size_t size) {
-  void* ptr;
-  return ply::MemPage::alloc(ptr, size, true) ? ptr : MFAIL;
+static PLY_INLINE void* ply_mmap(size_t size) {
+  char* ptr;
+  return ply::MemPage::alloc(ptr, size) ? ptr : MFAIL;
 }
 
 /* This function supports releasing coalesed segments */
 static PLY_INLINE int ply_munmap(void* ptr, size_t size) {
-  return ply::MemPage::free(ptr, size) ? 0 : -1;
+  return ply::MemPage::freeForDLMalloc((char*) ptr, size) ? 0 : -1;
 }
 
 #define MMAP_DEFAULT(s)             ply_mmap(s)
 #define MUNMAP_DEFAULT(a, s)        ply_munmap((a), (s))
-#define DIRECT_MMAP_DEFAULT(s)      ply_direct_mmap(s)
+#define DIRECT_MMAP_DEFAULT(s)      ply_mmap(s)
+
+#else /* WIN32 */
+
+/* Win32 MMAP via VirtualAlloc */
+static FORCEINLINE void* win32mmap(size_t size) {
+  void* ptr = VirtualAlloc(0, size, MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
+  return (ptr != 0)? ptr: MFAIL;
+}
+
+/* For direct MMAP, use MEM_TOP_DOWN to minimize interference */
+static FORCEINLINE void* win32direct_mmap(size_t size) {
+  void* ptr = VirtualAlloc(0, size, MEM_RESERVE|MEM_COMMIT|MEM_TOP_DOWN,
+                           PAGE_READWRITE);
+  return (ptr != 0)? ptr: MFAIL;
+}
+
+/* This function supports releasing coalesed segments */
+static FORCEINLINE int win32munmap(void* ptr, size_t size) {
+  MEMORY_BASIC_INFORMATION minfo;
+  char* cptr = (char*)ptr;
+  while (size) {
+    if (VirtualQuery(cptr, &minfo, sizeof(minfo)) == 0)
+      return -1;
+    if (minfo.BaseAddress != cptr || minfo.AllocationBase != cptr ||
+        minfo.State != MEM_COMMIT || minfo.RegionSize > size)
+      return -1;
+    if (VirtualFree(cptr, 0, MEM_RELEASE) == 0)
+      return -1;
+    cptr += minfo.RegionSize;
+    size -= minfo.RegionSize;
+  }
+  return 0;
+}
+
+#define MMAP_DEFAULT(s)             win32mmap(s)
+#define MUNMAP_DEFAULT(a, s)        win32munmap((a), (s))
+#define DIRECT_MMAP_DEFAULT(s)      win32direct_mmap(s)
+#endif /* WIN32 */
 
 #if HAVE_MREMAP
 #ifndef WIN32

repos/plywood/src/runtime/ply-runtime/memory/impl/MemPage_POSIX.cpp
@@ -1,0 +1,82 @@
+/*------------------------------------
+  ///\  Plywood C++ Framework
+  \\\/  https://plywood.arc80.com/
+------------------------------------*/
+
+#include <ply-runtime/Precomp.h>
+
+#if PLY_TARGET_POSIX
+
+#include <ply-runtime/memory/impl/MemPage_POSIX.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+namespace ply {
+
+const MemPage_POSIX::Info& MemPage_POSIX::getInfo() {
+    static Info info = []() -> Info {
+        long result = sysconf(_SC_PAGE_SIZE);
+        PLY_ASSERT(isPowerOf2(result));
+        return {(ureg) result, (ureg) result};
+    }();
+    return info;
+}
+
+bool MemPage_POSIX::alloc(char*& outAddr, uptr numBytes) {
+    PLY_ASSERT(isAlignedPowerOf2(numBytes, getInfo().allocationGranularity));
+
+    outAddr = (char*) mmap(0, numBytes, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    PLY_ASSERT(outAddr != MAP_FAILED);
+    return true;
+}
+
+bool MemPage_POSIX::reserve(char*& outAddr, uptr numBytes) {
+    PLY_ASSERT(isAlignedPowerOf2(numBytes, getInfo().allocationGranularity));
+
+#if PLY_KERNEL_LINUX
+    outAddr = (char*) mmap(0, numBytes, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    PLY_ASSERT(outAddr != MAP_FAILED);
+#else
+    PLY_FORCE_CRASH(); // Not supported yet
+#endif
+    return true;
+}
+
+void MemPage_POSIX::commit(char* addr, uptr numBytes) {
+    PLY_ASSERT(isAlignedPowerOf2((uptr) addr, getInfo().pageSize));
+    PLY_ASSERT(isAlignedPowerOf2(numBytes, getInfo().pageSize));
+
+#if PLY_KERNEL_LINUX
+    int rc = mprotect(addr, numBytes, PROT_READ | PROT_WRITE);
+    PLY_ASSERT(rc == 0);
+    PLY_UNUSED(rc);
+#else
+    PLY_FORCE_CRASH(); // Not supported yet
+#endif
+}
+
+void MemPage_POSIX::decommit(char* addr, uptr numBytes) {
+    PLY_ASSERT(isAlignedPowerOf2((uptr) addr, getInfo().pageSize));
+    PLY_ASSERT(isAlignedPowerOf2(numBytes, getInfo().pageSize));
+
+#if PLY_KERNEL_LINUX
+    int rc = madvise(addr, numBytes, MADV_DONTNEED);
+    PLY_ASSERT(rc == 0);
+    rc = mprotect(addr, numBytes, PROT_NONE);
+    PLY_ASSERT(rc == 0);
+    PLY_UNUSED(rc);
+#else
+    PLY_FORCE_CRASH(); // Not supported yet
+#endif
+}
+
+void MemPage_POSIX::free(char* addr, uptr numBytes) {
+    PLY_ASSERT(isAlignedPowerOf2((uptr) addr, getInfo().allocationGranularity));
+    PLY_ASSERT(isAlignedPowerOf2(numBytes, getInfo().allocationGranularity));
+
+    munmap(addr, numBytes);
+}
+
+} // namespace ply
+
+#endif // PLY_TARGET_POSIX

repos/plywood/src/runtime/ply-runtime/memory/impl/MemPage_POSIX.h
@@ -5,39 +5,23 @@
 #pragma once
 #include <ply-runtime/Core.h>
 #include <ply-platform/Util.h>
-#include <unistd.h>
-#include <sys/mman.h>
 
 namespace ply {
 
 class MemPage_POSIX {
 public:
     struct Info {
-        ureg allocationGranularity;
-        ureg pageSize;
+        uptr allocationGranularity;
+        uptr pageSize;
     };
 
-    static Info getInfoInternal() {
-        long result = sysconf(_SC_PAGE_SIZE);
-        PLY_ASSERT(isPowerOf2(result));
-        return {(ureg) result, (ureg) result};
-    }
+    static const Info& getInfo();
 
-    static const Info& getInfo() {
-        static Info info = getInfoInternal();
-        return info;
-    }
-
-    static bool alloc(void*& result, ureg size, bool topDownHint = false) {
-        result = mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-        PLY_ASSERT(result != MAP_FAILED);
-        return true;
-    }
-
-    static bool free(void* ptr, ureg size) {
-        munmap(ptr, size);
-        return true;
-    }
+    static bool alloc(char*& outAddr, uptr numBytes);
+    static bool reserve(char*& outAddr, uptr numBytes);
+    static void commit(char* addr, uptr numBytes);
+    static void decommit(char* addr, uptr numBytes);
+    static void free(char* addr, uptr numBytes);
 };
 
 } // namespace ply

repos/plywood/src/runtime/ply-runtime/memory/impl/MemPage_Win32.cpp
@@ -1,0 +1,84 @@
+/*------------------------------------
+  ///\  Plywood C++ Framework
+  \\\/  https://plywood.arc80.com/
+------------------------------------*/
+
+#include <ply-runtime/Precomp.h>
+
+#if PLY_TARGET_WIN32
+
+#include <ply-runtime/memory/impl/MemPage_Win32.h>
+
+namespace ply {
+
+const MemPage_Win32::Info& MemPage_Win32::getInfo() {
+    static Info info = []() -> Info {
+        SYSTEM_INFO sysInfo;
+        GetSystemInfo(&sysInfo);
+        PLY_ASSERT(isPowerOf2(sysInfo.dwAllocationGranularity));
+        PLY_ASSERT(isPowerOf2(sysInfo.dwPageSize));
+        return {sysInfo.dwAllocationGranularity, sysInfo.dwPageSize};
+    }();
+    return info;
+}
+
+bool MemPage_Win32::alloc(char*& outAddr, uptr numBytes) {
+    PLY_ASSERT(isAlignedPowerOf2(numBytes, getInfo().allocationGranularity));
+
+    DWORD type = MEM_RESERVE | MEM_COMMIT;
+    outAddr = (char*) VirtualAlloc(0, (SIZE_T) numBytes, type, PAGE_READWRITE);
+    return (outAddr != NULL);
+}
+
+bool MemPage_Win32::reserve(char*& outAddr, uptr numBytes) {
+    PLY_ASSERT(isAlignedPowerOf2(numBytes, getInfo().allocationGranularity));
+
+    DWORD type = MEM_RESERVE;
+    outAddr = (char*) VirtualAlloc(0, (SIZE_T) numBytes, type, PAGE_READWRITE);
+    return (outAddr != NULL);
+}
+
+void MemPage_Win32::commit(char* addr, uptr numBytes) {
+    PLY_ASSERT(isAlignedPowerOf2((uptr) addr, getInfo().pageSize));
+    PLY_ASSERT(isAlignedPowerOf2(numBytes, getInfo().pageSize));
+
+    DWORD type = MEM_COMMIT;
+    LPVOID result = VirtualAlloc(addr, (SIZE_T) numBytes, type, PAGE_READWRITE);
+    PLY_ASSERT(result != NULL);
+    PLY_UNUSED(result);
+}
+
+void MemPage_Win32::decommit(char* addr, uptr numBytes) {
+    PLY_ASSERT(isAlignedPowerOf2((uptr) addr, getInfo().pageSize));
+    PLY_ASSERT(isAlignedPowerOf2(numBytes, getInfo().pageSize));
+
+    DWORD type = MEM_COMMIT;
+    LPVOID result = VirtualAlloc(addr, (SIZE_T) numBytes, type, PAGE_READWRITE);
+    PLY_ASSERT(result != NULL);
+    PLY_UNUSED(result);
+}
+
+void MemPage_Win32::free(char* addr, uptr numBytes) {
+    PLY_ASSERT(isAlignedPowerOf2((uptr) addr, getInfo().allocationGranularity));
+    PLY_ASSERT(isAlignedPowerOf2(numBytes, getInfo().allocationGranularity));
+
+#if PLY_WITH_ASSERTS
+    {
+        // Must be entire reserved address space range
+        MEMORY_BASIC_INFORMATION memInfo;
+        SIZE_T rc = VirtualQuery(addr, &memInfo, sizeof(memInfo));
+        PLY_ASSERT(rc != 0);
+        PLY_UNUSED(rc);
+        PLY_ASSERT(memInfo.BaseAddress == addr);
+        PLY_ASSERT(memInfo.AllocationBase == addr);
+        PLY_ASSERT(memInfo.RegionSize <= numBytes);
+    }
+#endif
+    BOOL rc2 = VirtualFree(addr, 0, MEM_RELEASE);
+    PLY_ASSERT(rc2);
+    PLY_UNUSED(rc2);
+}
+
+} // namespace ply
+
+#endif // PLY_TARGET_WIN32

repos/plywood/src/runtime/ply-runtime/memory/impl/MemPage_Win32.h
@@ -8,49 +8,21 @@
 
 namespace ply {
 
-class MemPage_Win32 {
-public:
+struct MemPage_Win32 {
     struct Info {
-        ureg allocationGranularity;
-        ureg pageSize;
+        uptr allocationGranularity;
+        uptr pageSize;
     };
 
-    static Info getInfoInternal() {
-        SYSTEM_INFO sysInfo;
-        GetSystemInfo(&sysInfo);
-        PLY_ASSERT(isPowerOf2(sysInfo.dwAllocationGranularity));
-        PLY_ASSERT(isPowerOf2(sysInfo.dwPageSize));
-        return {sysInfo.dwAllocationGranularity, sysInfo.dwPageSize};
-    }
+    static const Info& getInfo();
 
-    static const Info& getInfo() {
-        static Info info = getInfoInternal();
-        return info;
-    }
-
-    static bool alloc(void*& result, ureg size, bool topDownHint = false) {
-        DWORD type = MEM_RESERVE | MEM_COMMIT;
-        if (topDownHint)
-            type |= MEM_TOP_DOWN;
-        result = VirtualAlloc(0, (SIZE_T) size, type, PAGE_READWRITE);
-        return (result != NULL);
-    }
-
-    static bool free(void* ptr, ureg size) {
-        MEMORY_BASIC_INFORMATION minfo;
-        while (sreg(size) > 0) {
-            if (VirtualQuery((LPCVOID) ptr, &minfo, sizeof(minfo)) == 0)
-                return false;
-            if (minfo.BaseAddress != ptr || minfo.AllocationBase != ptr ||
-                minfo.State != MEM_COMMIT || minfo.RegionSize > size)
-                return false;
-            if (VirtualFree(ptr, 0, MEM_RELEASE) == 0)
-                return false;
-            ptr = PLY_PTR_OFFSET(ptr, minfo.RegionSize);
-            size -= minfo.RegionSize;
-        }
-        return true;
-    }
+    static bool alloc(char*& baseAddr, uptr numBytes);
+    static bool reserve(char*& baseAddr, uptr numBytes);
+    static void commit(char* addr, uptr numBytes);
+    static void decommit(char* addr, uptr numBytes);
+    // Note that unlike MemPage_POSIX, MemPage_Win32::free() is only able to free a single entire
+    // region returned by alloc() or reserve(). For this reason, Heap_DL doesn't use MemPage_Win32.
+    static void free(char* baseAddr, uptr numBytes);
 };
 
 } // namespace ply

commit 296d85eae01a3dfd2454bd2b8014c6fefda396ca

Fix compile error affecting non-Windows platforms
Was accidentally introduced in 036ccebc8560d952803c5568a001304c93e0a022

repos/plywood/src/runtime/ply-runtime/memory/impl/Heap_DL.cpp
@@ -193,7 +193,8 @@ static PLY_INLINE void* ply_mmap(size_t size) {
 
 /* This function supports releasing coalesed segments */
 static PLY_INLINE int ply_munmap(void* ptr, size_t size) {
-  return ply::MemPage::freeForDLMalloc((char*) ptr, size) ? 0 : -1;
+  ply::MemPage::free((char*) ptr, size);
+  return 0;
 }
 
 #define MMAP_DEFAULT(s)             ply_mmap(s)