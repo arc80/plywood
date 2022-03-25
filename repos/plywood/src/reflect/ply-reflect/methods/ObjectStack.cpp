71180cb509f4e5bee097c2d8c5b57bd682377150
Jeff Preshing <filter-github@preshing.com>
2022-03-25 15:26:43 -0400

    Fix bug in ObjectStack

--- repos/plywood/src/reflect/ply-reflect/methods/ObjectStack.cpp
+++ repos/plywood/src/reflect/ply-reflect/methods/ObjectStack.cpp
@@ -34,13 +34,14 @@ void ObjectStack::deleteRange(const ObjectStack::Boundary& from, const WeakSeque
     WeakSequenceRef<AnyObject> end = this->items.end();
     for (; cur != end; ++cur) {
         u32 numBytes = cur->type->fixedSize;
-        if (safeDemote<u32>(dst.block->end() - dst.byte) > numBytes) {
+        if (safeDemote<u32>(dst.block->end() - dst.byte) < numBytes) {
             // FIXME: Handle objects larger than the block size
             PLY_ASSERT(dst.block->nextBlock->blockSize >= numBytes);
             dst = {dst.block->nextBlock, dst.block->bytes};
         }
         // FIXME: Handle alignment
         memmove(dst.byte, cur->data, cur->type->fixedSize);
+        cur->data = dst.byte;
         dst.byte += cur->type->fixedSize;
     }
 
