904a8b90fdc7c7b4a86e102c7a70b441edb3b0b8
Jeff Preshing <filter-github@preshing.com>
2022-04-20 13:34:36 -0400

    Fix missing precompiled header includes for Win32

--- repos/plywood/src/runtime/ply-runtime/container/BigPool.cpp
+++ repos/plywood/src/runtime/ply-runtime/container/BigPool.cpp
@@ -2,6 +2,7 @@
   ///\  Plywood C++ Framework
   \\\/  https://plywood.arc80.com/
 ------------------------------------*/
+#include <ply-runtime/Precomp.h>
 #include <ply-runtime/container/BigPool.h>
 #include <ply-runtime/memory/MemPage.h>
 
--- repos/plywood/src/runtime/ply-runtime/container/InternedStrings.cpp
+++ repos/plywood/src/runtime/ply-runtime/container/InternedStrings.cpp
@@ -2,6 +2,7 @@
   ///\  Plywood C++ Framework
   \\\/  https://plywood.arc80.com/
 ------------------------------------*/
+#include <ply-runtime/Precomp.h>
 #include <ply-runtime/Core.h>
 #include <ply-runtime/container/InternedStrings.h>
 
