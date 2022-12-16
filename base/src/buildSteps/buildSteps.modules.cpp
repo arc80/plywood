4232be2bba330bf962e81d913287f57899b72894
Jeff Preshing <filter-github@preshing.com>
2022-12-16 07:38:38 -0500

    Delete *.modules.cpp files

--- base/src/buildSteps/buildSteps.modules.cpp
+++
@@ -1,9 +0,0 @@
-#include <ply-build-repo/Module.h>
-
-// [ply module="buildSteps"]
-void module_buildSteps(ModuleArgs* args) {
-    args->addSourceFiles("buildSteps", false);
-    args->addIncludeDir(Visibility::Public, ".");
-    args->addTarget(Visibility::Public, "runtime");
-    args->addTarget(Visibility::Public, "pylon");
-}
